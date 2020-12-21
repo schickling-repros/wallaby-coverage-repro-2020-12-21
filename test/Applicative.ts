43cdce731820216220695678561507b3c512ae42
Johannes Schickling <schickling.j@gmail.com>
2020-12-21 10:15:01 +0100

    Added example test

--- test/Applicative.ts
+++ test/Applicative.ts
@@ -3,6 +3,32 @@ import { getApplicativeComposition } from '../src/Applicative'
 import * as A from '../src/ReadonlyArray'
 import * as O from '../src/Option'
 
+// new code start
+import * as E from '../src/Either'
+import { pipe } from '../src/function'
+
+it('should notice curried functions', () => {
+  const result = pipe(
+    E.right([
+      [1, 'a'],
+      [2, 'b'],
+      [3, 'c']
+    ] as [number, string][]),
+    E.chain(_ => E.right(_)),
+    E.map(A.unzip)
+  )
+  assertG(E.isRight, result)
+  expect(result.right).toEqual([[1, 2, 3], ['a', 'b', 'c']])
+})
+
+function assertG<T>(guard: (o: any) => o is T, o: any): asserts o is T {
+  if (!guard(o)) {
+    console.error(o)
+    throw new Error() // or add param for custom error
+  }
+}
+// new code end
+
 describe('Applicative', () => {
   it('getApplicativeComposition', () => {
     const arrayOption = getApplicativeComposition(A.Applicative, O.Applicative)
